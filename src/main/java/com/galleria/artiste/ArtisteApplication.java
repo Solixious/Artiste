1354f7d553d15926b76273c2de51f0356da0909f
Pratyush Choudhary <solixious@gmail.com>
2019-12-18 19:57:55 +0530

    Initial Commit

---
+++ src/main/java/com/galleria/artiste/ArtisteApplication.java
@@ -0,0 +1,13 @@
+package com.galleria.artiste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtisteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtisteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galleria/artiste/ArtisteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galleria.artiste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtisteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
